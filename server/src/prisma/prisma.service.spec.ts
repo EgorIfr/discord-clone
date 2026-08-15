0ccf6d19ff3cb30c7d5d40ea97013df1debc6319
Egorlfr <asd141299@gmail.com>
2026-08-15 09:56:32 +0300

    add prisma modules

---
+++ server/src/prisma/prisma.service.spec.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from './prisma.service';
+import { describe, beforeEach, it } from 'node:test';
+
+describe('PrismaService', () => {
+  let service: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [PrismaService],
+    }).compile();
+
+    service = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
